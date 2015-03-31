d0b2759db8555a13844d891387c5e7cf6184256c
neonnds <neonnds@gmail.com>
2015-04-01 01:08:38 +1100

    Fixed up config and starting index

--- sites/elastic-core/index.js
+++ sites/elastic-core/index.js
@@ -53,13 +53,6 @@ framework.once('load', function() {
 	};
 });
 
-var options = {};
-options.ip = '127.0.0.1';
-options.port = parseInt(process.argv[2]);
-options.config = { name: 'Bomblah' };
-//options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
-
-framework.http('debug', options);
+framework.http('debug');
 
 console.log('****** Started ******');
-console.log('****** http://{0}:{1}/ ******'.format(framework.ip, framework.port));
